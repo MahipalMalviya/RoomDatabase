36ea4009fcebb20881c117fe36408496f5763b79
MahipalMalviya <mmalviya@softcell.com>
2018-05-02 19:54:16 +0530

    1. Ui and interface dao completed.

---
+++ app/src/main/java/mahipal/roomdatabase/AppDatabase.kt
@@ -0,0 +1,10 @@
+package mahipal.roomdatabase
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+
+//@Database(entities = {User}, version = 1)
+abstract class AppDatabase: RoomDatabase(){
+
+
+}
